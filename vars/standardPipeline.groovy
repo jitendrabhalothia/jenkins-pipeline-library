04f143aa360af9c59f62dbac0b5585ddc124d698
Jitendra Singh <jitendra@Jitendras-MacBook-Pro.local>
2018-02-13 13:51:49 +0530

    Adjusted

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -21,33 +21,126 @@ def call(body) {
             }
 
             stages {
-                stage ('dev05-rabbitmq') {
-                    //when {
-                        // Only say hello if a "rabbitmq" is requested
-                       // expression { params.REQUESTED_ACTION == 'rabbitmq' || params.REQUESTED_ACTION == 'all'}
-                    //}
+                stage ('rabbitmq') {
+                    
                     steps {
                         script {
                             if (params.REQUESTED_ACTION == 'rabbitmq'){
-                                echo 'Depoying rabbitmq for dev05-rabbitmq'
+                                echo 'Depoying rabbitmq'
                                 sh "cd ${PLAYBOOK_PATH}/${config.folderName} && cp ~/ansible.cfg ansible.cfg && sudo ansible-playbook -i ${PLAYBOOK_PATH}/${config.folderName}/inventory/${config.envName} ${PLAYBOOK_PATH}/${config.folderName}/rabbitmq.yml --tags update --vault-password-file  ~/.agv"
                             }
                         }
                     }
                 }
-                stage ('dev05-redis') {
-                    //when {
-                        // Only say hello if a "redis is requested
-                        //expression { params.REQUESTED_ACTION == 'redis'  || params.REQUESTED_ACTION == 'all'}
-                    //}
+                stage ('redis') {
+                    
                     steps {
                         script {
                             if (params.REQUESTED_ACTION == 'redis'){
-                                echo 'Depoying redis for dev05'
+                                echo 'Depoying redis'
                                 sh "cd ${PLAYBOOK_PATH}/${config.folderName} && cp ~/ansible.cfg ansible.cfg && sudo ansible-playbook -i ${PLAYBOOK_PATH}/${config.folderName}/inventory/${config.envName} ${PLAYBOOK_PATH}/${config.folderName}/redis.yml --tags update --vault-password-file  ~/.agv"
                             }
                         }
                         
+                    }
+                }
+                stage ('tusker') {
+                    
+                    steps {
+                        script {
+                            if (params.REQUESTED_ACTION == 'tusker'){
+                                echo 'Depoying  tusker'
+                                sh "cd ${PLAYBOOK_PATH}/${config.folderName} && cp ~/ansible.cfg ansible.cfg && sudo ansible-playbook -i ${PLAYBOOK_PATH}/${config.folderName}/inventory/${config.envName} ${PLAYBOOK_PATH}/${config.folderName}/tusker.yml --tags update --vault-password-file  ~/.agv"
+                            }
+                        }
+                    }
+                }
+                stage ('pheme') {
+                    
+                    steps {
+                        script {
+                            if (params.REQUESTED_ACTION == 'pheme'){
+                                echo 'Depoying  pheme'
+                                sh "cd ${PLAYBOOK_PATH}/${config.folderName} && cp ~/ansible.cfg ansible.cfg && sudo ansible-playbook -i ${PLAYBOOK_PATH}/${config.folderName}/inventory/${config.envName} ${PLAYBOOK_PATH}/${config.folderName}/pheme.yml --tags update --vault-password-file  ~/.agv"
+                            }
+                        }
+                    }
+                }
+                stage ('droms') {
+                    
+                    steps {
+                        script {
+                            if (params.REQUESTED_ACTION == 'droms'){
+                                echo 'Depoying droms'
+                                sh "cd ${PLAYBOOK_PATH}/${config.folderName} && cp ~/ansible.cfg ansible.cfg && sudo ansible-playbook -i ${PLAYBOOK_PATH}/${config.folderName}/inventory/${config.envName} ${PLAYBOOK_PATH}/${config.folderName}/droms.yml --tags update --vault-password-file  ~/.agv"
+                            }
+                        }
+                    }
+                }
+                stage ('openadr2b') {
+                    
+                    steps {
+                        script {
+                            if (params.REQUESTED_ACTION == 'openadr2b'){
+                                echo 'Depoying openadr2b'
+                                sh "cd ${PLAYBOOK_PATH}/${config.folderName} && cp ~/ansible.cfg ansible.cfg && sudo ansible-playbook -i ${PLAYBOOK_PATH}/${config.folderName}/inventory/${config.envName} ${PLAYBOOK_PATH}/${config.folderName}/openadr2b.yml --tags update --vault-password-file  ~/.agv"
+                            }
+                        }
+                    }
+                }
+                stage ('ceep') {
+                    
+                    steps {
+                        script {
+                            if (params.REQUESTED_ACTION == 'ceep'){
+                                echo 'Depoying ceep'
+                                sh "cd ${PLAYBOOK_PATH}/${config.folderName} && cp ~/ansible.cfg ansible.cfg && sudo ansible-playbook -i ${PLAYBOOK_PATH}/${config.folderName}/inventory/${config.envName} ${PLAYBOOK_PATH}/${config.folderName}/ceep.yml --tags update --vault-password-file  ~/.agv"
+                            }
+                        }
+                    }
+                }
+                stage ('dianoga') {
+                    
+                    steps {
+                        script {
+                            if (params.REQUESTED_ACTION == 'dianoga'){
+                                echo 'Depoying dianoga'
+                                sh "cd ${PLAYBOOK_PATH}/${config.folderName} && cp ~/ansible.cfg ansible.cfg && sudo ansible-playbook -i ${PLAYBOOK_PATH}/${config.folderName}/inventory/${config.envName} ${PLAYBOOK_PATH}/${config.folderName}/dianoga.yml --tags update --vault-password-file  ~/.agv"
+                            }
+                        }
+                    }
+                }
+                stage ('rtcc') {
+                    
+                    steps {
+                        script {
+                            if (params.REQUESTED_ACTION == 'rtcc'){
+                                echo 'Depoying rtcc'
+                                sh "cd ${PLAYBOOK_PATH}/${config.folderName} && cp ~/ansible.cfg ansible.cfg && sudo ansible-playbook -i ${PLAYBOOK_PATH}/${config.folderName}/inventory/${config.envName} ${PLAYBOOK_PATH}/${config.folderName}/rtcc.yml --tags update --vault-password-file  ~/.agv"
+                            }
+                        }
+                    }
+                }
+                stage ('cascade') {
+                    
+                    steps {
+                        script {
+                            if (params.REQUESTED_ACTION == 'cascade'){
+                                echo 'Depoying cascade'
+                                sh "cd ${PLAYBOOK_PATH}/${config.folderName} && cp ~/ansible.cfg ansible.cfg && sudo ansible-playbook -i ${PLAYBOOK_PATH}/${config.folderName}/inventory/${config.envName} ${PLAYBOOK_PATH}/${config.folderName}/cascade.yml --tags update --vault-password-file  ~/.agv"
+                            }
+                        }
+                    }
+                }
+                stage ('fam-backend') {
+                    
+                    steps {
+                        script {
+                            if (params.REQUESTED_ACTION == 'fam-backend'){
+                                echo 'Depoying fam-backend'
+                                sh "cd ${PLAYBOOK_PATH}/${config.folderName} && cp ~/ansible.cfg ansible.cfg && sudo ansible-playbook -i ${PLAYBOOK_PATH}/${config.folderName}/inventory/${config.envName} ${PLAYBOOK_PATH}/${config.folderName}/fam-backend.yml --tags=configs,restart --vault-password-file  ~/.agv"
+                            }
+                        }
                     }
                 }
 
@@ -56,3 +149,4 @@ def call(body) {
             }
         }
     }
+
